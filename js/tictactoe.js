4d06ca68d1951f421fbd73cbbef574fdf95fc574
Jonathan Dai <jonathanjddai@gmail.com>
2016-03-14 18:59:49 -0700

    text update

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -1,4 +1,19 @@
 console.log("hey")
 
+var refresh = document.getElementById("Clear");
+
+refresh.addEventListener("click", function() {
+  a1.innerHTML =""
+  a2.innerHTML =""
+  a3.innerHTML =""
+  b1.innerHTML =""
+  b2.innerHTML =""
+  b3.innerHTML =""
+  c1.innerHTML =""
+  c2.innerHTML =""
+  c3.innerHTML =""
+})
+
+
 
 
